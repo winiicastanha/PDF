d07ae936c7789991fb42be14bc3beb694db27fb9
Winicius Machado Castanha <winiicastanha@gmail.com>
2024-06-04 17:42:34 -0300

    Update neweditor.tsx

--- login-app/src/neweditor.tsx
+++ login-app/src/neweditor.tsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import '@toast-ui/editor/dist/toastui-editor.css';
-import { Editor } from '@toast-ui/react-editor';
+import { Editor as ToastEditor, EditorProps } from '@toast-ui/react-editor';
 import axios from 'axios';
 
 const theme = createTheme({
@@ -12,28 +12,32 @@ const theme = createTheme({
 });
 
 const NewEditor = () => {
-  const editorRef = useRef();
+  const editorRef = useRef<ToastEditor>(null);
 
   const handleSave = async () => {
-    const editorInstance = editorRef.current.getInstance();
-    const content = editorInstance.getMarkdown();
+    if (editorRef.current) {
+      const editorInstance = editorRef.current.getInstance();
+      const content = editorInstance.getMarkdown();
 
-    try {
-      const response = await axios.get('http://localhost:3001/new-editor', { content });
-      console.log('Resposta do servidor:', response.data);
-    } catch (error) {
-      console.error('Erro ao salvar o conteúdo:', error);
+      try {
+        const response = await axios.post('http://localhost:3001/new-editor', { content });
+        console.log('Resposta do servidor:', response.data);
+      } catch (error) {
+        console.error('Erro ao salvar o conteúdo:', error);
+      }
+    } else {
+      console.error('Editor não está corretamente referenciado');
     }
   };
 
   return (
     <ThemeProvider theme={theme}>
       <div>
-        <Editor
+        <ToastEditor
           ref={editorRef}
           initialValue="hello react editor world!"
           previewStyle="vertical"
-          height="600px"
+          height="500px"
           initialEditType="markdown"
           useCommandShortcut={true}
         />
